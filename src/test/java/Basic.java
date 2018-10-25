f6d81d8e62f789c3fd1743ace09c4a29c1e3322e
Elena Chepizhnaya <43644856+Kelenna@users.noreply.github.com>
2018-10-25 18:43:49 +0300

    Basic hello world search

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.List;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,20 +9,24 @@ public class Basic {
     private WebDriver driver;
     private String baseUrl;
 
-
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", getAppPath());
         driver = new ChromeDriver();
-        baseUrl = "https://www.google.com.ua/";
+        baseUrl = "https://www.google.com";
     }
-
 
     @Test
     public void testSearch() {
         driver.get(baseUrl);
-        driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("Selenium Webdriver");
-        driver.findElement(By.xpath("//input[@value=\"Поиск в Google\"]")).click();
+        driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("Hello world!");
+        driver.findElement(By.xpath("//input[@value=\"Поиск в Google\"]")).submit();
+        WebElement resultsArea = driver.findElement(By.xpath("//*[@id='rso']/div[2]/div"));
+        List<WebElement> resultList = resultsArea.findElements(By.className("g"));
+        resultList.get(4).findElement(By.tagName("a")).click();
+
+        List<WebElement> helloCheck = driver.findElements(By.xpath("//*[contains(text(), 'hello world')]"));
+        Assert.assertTrue("Not Found!", helloCheck.size() > 0);
     }
 
     private final String getAppPath() {
